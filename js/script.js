5983ed31be63337ab90be7cec2e73a4c9a44f785
Rafael Neri <rafaelrvcn@outlook.com>
2020-10-23 16:24:40 -0300

    add php for heroku

--- js/script.js
+++ js/script.js
@@ -1,96 +1,60 @@
-const fields = document.querySelectorAll("[required]")
-
-function validateField(field) {
-    function verifyErrors() {
-        let foundError = false
-        console.log(field);
-        for(let error in field.validity){
-            if (field.validity[error] && !field.validity.valid) {
-                foundError = error
-            }
-        }
-        return foundError
-    }
-
-    function customMessage(typeError) {
-        const messages = {
-
-            text: {
-                valueMissing: "Preencha"
-            },
-            email: {
-                valueMissing: "Email Obrig",
-                typeMismatch: "Preencha"
-            },
-            password: {
-                valueMissing: "Password cannot be empty"
-            }
-        }
-
-        return messages[field.type][typeError]
-    }
-
-    function setMessage(message){
-        const smallError = field.parentNode.querySelector("small.msg-error")
-        if (message) {
-            smallError.classList.add("active")
-            smallError.innerHTML = message
-        } else {
-            smallError.classList.remove("active")
-            smallError.innerHTML = ""
-        }
-    }
-
-    return function() {
-
-        const error = verifyErrors()
-        console.log();
-        if(error) { 
-            const message = customMessage(error)
-
-            field.style.borderColor = "hsl(0, 100%, 74%)"
-            setMessage(message)            
-        } else {
-            field.style.borderColor = "hsl(249, 10%, 26%)"
-            setMessage()
-        }
-    }
-}
-
-function validation(event) {
-
-    const field = event.target
-    const validation = validateField(field)
-
-    validation()
-
-}
-
-for(let field of fields){
-    field.addEventListener("invalid", event => {
-        event.preventDefault() 
-
-        validation(event)
-    })
-    field.addEventListener("blur", validation)
-}
-
-
-
-
-
-
-
-
-
-
-
-
-const firstname = document.querySelector("#first-name")
-const lastname = document.querySelector("#last-name")
+const fname = document.querySelector("#first-name")
+const lname = document.querySelector("#last-name")
 const email = document.querySelector("#email")
-const password = document.querySelector("#password")
-const efirstname = document.querySelector("#error-firstn")
-const elastname = document.querySelector("#error-lastn")
-const erroremail = document.querySelector("#error-email")
-const epassword = document.querySelector("#error-pass")
+const pass = document.querySelector("#password")
+const form = document.querySelector(".signup")
+const efname = document.querySelector("#error-firstn")
+const elname = document.querySelector("#error-lastn")
+const eremail = document.querySelector("#error-email") 
+const epass = document.querySelector("#error-pass")
+
+form.addEventListener("submit", event => {
+    event.preventDefault()
+    let errorsf = ""
+    let errorsl = ""
+    let errorse = ""
+    let errorsp = ""
+    let entrarf = false
+    let entrarl = false
+    let entrare = false
+    let entrarp = false
+
+    let regexEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*(\.\w{2,4})+$/
+    if(fname.value.length < 3) {
+        errorsf += `First name cannot be empty`
+        entrarf = true
+        fname.parentElement.querySelector(".icon").classList.add("error")
+    }
+    if(lname.value.length < 2) {
+        errorsl += `Last name cannot be empty`
+        entrarl = true
+        lname.parentElement.querySelector(".icon").classList.add("error")
+    }
+    if(!regexEmail.test(email.value)){
+        errorse += `Looks like this is not an email`
+        entrare = true
+        email.parentElement.querySelector(".icon").classList.add("error")
+    }
+    if(pass.value.length < 6){
+        errorsp += `Password cannot be empty`
+        entrarp = true
+        pass.parentElement.querySelector(".icon").classList.add("error")
+    }
+    if(entrarf) {
+        efname.innerHTML = errorsf
+        fname.style.borderColor = "hsl(0, 100%, 74%)"
+
+    }
+    if(entrarl) {
+        elname.innerHTML = errorsl
+        lname.style.borderColor = "hsl(0, 100%, 74%)"
+    }
+    if(entrare) {
+        eremail.innerHTML = errorse
+        email.style.borderColor = "hsl(0, 100%, 74%)"
+    }
+    if(entrarp) {
+        epass.innerHTML = errorsp
+        pass.style.borderColor = "hsl(0, 100%, 74%)"
+    }
+})
